1eaf9d722245f19a04b0dbe912af786c9d5e1fd9
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-01-11 01:22:42 -0500

    Added basic math functions to operators

--- include/math/vector2.h
+++ include/math/vector2.h
@@ -13,20 +13,78 @@ namespace nautical
                             struct { T x; T y;};
                             T components[2];
                     };
+                    Vector2() : x(0), y(0) {};
                     Vector2(T x, T y) : x(x), y(y) {};
                     Vector2(T* data)
                     {
                             memcpy(components, data, sizeof(T) * 2);
                     };
 
-                    inline T length()
+                    inline T lengthSquared() const
                     {
-                        return sqrt(x*x + y*y);
+                        return x*x + y*y;
                     }
 
-                    inline T direction()
+                    inline T length() const
+                    {
+                        return sqrt(lengthSquared());
+                    }
+
+                    inline T direction() const
                     {
                         return atan2(y, x);
+                    }
+
+                    inline Vector2<T> operator+(const Vector2<T>& b) const
+                    {
+                        Vector2<T> out;
+                        out.x = x + b.x;
+                        out.y = y + b.y;
+                        return out;
+                    }
+
+                    inline Vector2<T>& operator+=(const Vector2<T> rhs)
+                    {
+                        return *this + rhs;
+                    }
+
+                    inline Vector2<T> operator-(const Vector2<T>& b) const
+                    {
+                        Vector2<T> out;
+                        out.x = x - b.x;
+                        out.y = y - b.y;
+                        return out;
+                    }
+
+                    inline Vector2<T>& operator-=(const Vector2<T> rhs)
+                    {
+                        return *this - rhs;
+                    }
+
+                    inline Vector2<T> operator*(const Vector2<T>& b) const
+                    {
+                        Vector2<T> out;
+                        out.x = x * b.x;
+                        out.y = y * b.y;
+                        return out;
+                    }
+
+                    inline Vector2<T>& operator*=(const Vector2<T> rhs)
+                    {
+                        return *this * rhs;
+                    }
+
+                    inline Vector2<T> operator/(const Vector2<T>& b) const
+                    {
+                        Vector2<T> out;
+                        out.x = x / b.x;
+                        out.y = y / b.y;
+                        return out;
+                    }
+
+                    inline Vector2<T>& operator/-(const Vector2<T> rhs)
+                    {
+                        return *this / rhs;
                     }
         };
     }
