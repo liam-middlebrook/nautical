2a0b49b7902f4c8418789552a1537ec17d181361
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-01-19 18:17:03 -0500

    Added basic math operation tests for vector2

--- tests/vector2.cpp
+++ tests/vector2.cpp
@@ -1,20 +1,61 @@
 #include "vector2.h"
 #include <stdio.h>
 
-void print_vector(nautical::math::Vector2<float> vector)
+using namespace nautical::math;
+
+void print_vector(Vector2<float> vector)
 {
     printf("Vector: X: %f Y: %f", vector.x, vector.y);
 }
 
-
 void test_vector2_add(void)
 {
-    // Lets pretend addition works for now
-
-    using namespace nautical::math;
-
     Vector2<float> vec1(15.0f, -30.0f);
     Vector2<float> vec2(28.0f, 14.4);
 
-    CU_ASSERT((vec1 + vec2) == Vector2<float>(43.0f, -15.6f));
+    Vector2<float> answer(43.0f, -15.6f);
+
+    CU_ASSERT((vec1 + vec2) == answer);
+
+    vec1 += vec2;
+    CU_ASSERT(vec1 == answer);
+}
+
+void test_vector2_subtract(void)
+{
+    Vector2<float> vec1(15.0f, -30.0f);
+    Vector2<float> vec2(28.0f, 14.4);
+
+    Vector2<float> answer(-13.0f, -44.4f);
+
+    CU_ASSERT((vec1 - vec2) == answer);
+
+    vec1 -= vec2;
+    CU_ASSERT(vec1 == answer);
+}
+
+void test_vector2_multiply(void)
+{
+    Vector2<float> vec1(15.0f, -30.0f);
+    Vector2<float> vec2(28.0f, 14.4);
+
+    Vector2<float> answer(420.0f, -432.0f);
+
+    CU_ASSERT((vec1 * vec2) == answer);
+
+    vec1 *= vec2;
+    CU_ASSERT(vec1 == answer);
+}
+
+void test_vector2_divide(void)
+{
+    Vector2<float> vec1(15.0f, -30.0f);
+    Vector2<float> vec2(5.0f, 6.0f);
+
+    Vector2<float> answer(3.0f, -5.0f);
+
+    CU_ASSERT((vec1 / vec2) == answer);
+
+    vec1 /= vec2;
+    CU_ASSERT(vec1 == answer);
 }
--- tests/vector2.h
+++ tests/vector2.h
@@ -10,11 +10,17 @@ typedef struct {
 } TestDef;
 
 void test_vector2_add(void);
+void test_vector2_subtract(void);
+void test_vector2_multiply(void);
+void test_vector2_divide(void);
 
 static TestDef tests_vector2[] = {
     {"Test Vector2 Add", test_vector2_add},
+    {"Test Vector2 Subtract", test_vector2_subtract},
+    {"Test Vector2 Multiply", test_vector2_multiply},
+    {"Test Vector2 Divide", test_vector2_divide},
 };
 
-static size_t tests_vector2_count = 1;
+static size_t tests_vector2_count = 4;
 
 #endif
