afd2887cb6c7b245992d6e4966f9c5b509b2d46b
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-01-21 13:48:56 -0500

    cleanup unneeded code

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <CUnit/Basic.h>
 
-#include "vector2.h"
-#include "vector3.h"
 #include "test_list.h"
 
 int main(int argc, char** argv)
@@ -31,39 +29,7 @@ int main(int argc, char** argv)
             }
         }
     }
-/*
-    vector2_suite = CU_add_suite("Vector2", NULL, NULL);
-    if(vector2_suite == NULL)
-    {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
 
-    for(size_t i = 0; i < tests_vector2_count; ++i)
-    {
-        if(CU_add_test(vector2_suite, tests_vector2[i].name, tests_vector2[i].func) == NULL)
-        {
-            CU_cleanup_registry();
-            return CU_get_error();
-        }
-    }
-
-    vector3_suite = CU_add_suite("Vector3", NULL, NULL);
-    if(vector3_suite == NULL)
-    {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    for(size_t i = 0; i < tests_vector3_count; ++i)
-    {
-        if(CU_add_test(vector3_suite, tests_vector3[i].name, tests_vector3[i].func) == NULL)
-        {
-            CU_cleanup_registry();
-            return CU_get_error();
-        }
-    }
-*/
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
 
--- tests/test.h
+++ tests/test.h
@@ -14,16 +14,4 @@ typedef struct {
     size_t count;
 } SuiteDef;
 
-// Begin Test Suite Declarations
-/*
-#include "vector2.h"
-#include "vector3.h"
-
-static SuiteDef suites[] = {
-    {"Vector2", tests_vector2, 7},
-    {"Vector3", tests_vector3, 8},
-};
-
-static size_t suite_count = 2;
-*/
 #endif
