f0d6800ad1faa9f478390c226a06b6e2dad2799c
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-02-24 09:01:02 -0500

    Implemented GameObject

--- include/gameobject.h
+++ include/gameobject.h
@@ -9,18 +9,29 @@ namespace nautical
     class GameObject
     {
     public:
+        // World Transformation for GameObject
         Transform<float> transform;
 
         GameObject(char* name);
 
         virtual ~GameObject();
 
+        // Init Function (called at object creation)
+        virtual void init();
+
+        // Update Function (called once per gameloop)
         virtual void update();
 
+        // LateUpdate function (called once per gameloop)
         virtual void lateUpdate();
 
+        // Adds a component with the specified name to the object
         void addComponent(char* name, GameComponent* component);
 
+        // Adds a script with the specified name to the object
+        void addScript(char* name, NauticalScript* script);
+
+        // Retrives a component from the gameobject
         GameComponent* getComponent(char* name);
 
     private:
--- src/gameobject.cpp
+++ src/gameobject.cpp
@@ -12,23 +12,73 @@ GameObject::~GameObject()
     // Unload Constructor Stuff
 }
 
+void GameObject::init()
+{
+    // Init all scripts
+    for(auto& script : _scripts)
+    {
+        script.init();
+    }
+
+    // Init all components
+    for(auto& component : _components)
+    {
+        component.init();
+    }
+
+    // Init all children
+    for(auto& child : _children)
+    {
+        child.init();
+    }
+}
+
 void GameObject::update()
 {
     // Update all scripts
+    for(auto& script : _scripts)
+    {
+        script.update();
+    }
+
     // Update all components
+    for(auto& component : _components)
+    {
+        component.update();
+    }
+
     // Update all children
+    for(auto& child : _children)
+    {
+        child.update();
+    }
 }
 
 void GameObject::lateUpdate()
 {
     // Update all scripts
+    for(auto& script : _scripts)
+    {
+        script.lateUpdate();
+    }
+
     // Update all children
+    for(auto& child : _children)
+    {
+        child.lateUpdate();
+    }
 }
 
 void GameObject::addComponent(char* name, GameComponent* component)
 {
     // Insert component into dictionary with name (hashed)
     _components.insert(std::make_pair(_hashAlg(name), component));
+}
+
+void GameObject::addScript(char* name, NauticalScript* script)
+{
+    // Insert script into dictionary with name (hashed)
+    _scripts.insert(std::make_pair(_hashAlg(name), script));
 }
 
 GameComponent* GameObject::getComponent(char* name)
