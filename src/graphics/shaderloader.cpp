7dc9de178e6bbe02a599f9b334ccb9bdfa932a3f
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-03-25 21:08:56 -0400

    changed shader file loading to be RAII

--- src/graphics/shaderloader.cpp
+++ src/graphics/shaderloader.cpp
@@ -8,28 +8,6 @@
 using namespace nautical;
 using namespace nautical::graphics;
 
-std::string* loadFile(const char* fileLoc)
-{
-    std::string* file = new std::string();
-    std::ifstream fs(fileLoc, std::ios::in);
-
-    std::string line = "";
-
-    if(!fs.is_open())
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    while(!fs.eof())
-    {
-        std::getline(fs, line);
-        file->append(line + "\n");
-    }
-
-    fs.close();
-    return file;
-}
-
 ShaderLoader::ShaderLoader()
 {
 }
@@ -41,28 +19,29 @@ ShaderLoader::~ShaderLoader()
 GLuint ShaderLoader::loadShader(const char* vertLoc, const char* fragLoc)
 {
     // Load shader and shit
-    std::string* file = loadFile(vertLoc);
 
-    const char* vertexChars = file->c_str();
+    std::ifstream vertIn(vertLoc);
+    std::string vertexStr((std::istreambuf_iterator<char>(vertIn)),
+                            std::istreambuf_iterator<char>());
+
+    const char* vertexChars = vertexStr.c_str();
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexChars, NULL);
     glCompileShader(vertexShader);
 
-    delete file;
-
     GLint status;
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
     printf("Vertex Shader Status %d\n", status);
 
-    file = loadFile(fragLoc);
-    const char* fragmentChars = file->c_str();
+    std::ifstream fragIn(fragLoc);
+    std::string fragStr((std::istreambuf_iterator<char>(fragIn)),
+                            std::istreambuf_iterator<char>());
+    const char* fragmentChars = fragStr.c_str();
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentChars, NULL);
     glCompileShader(fragmentShader);
-
-    delete file;
 
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
     printf("Fragment Shader Status %d\n", status);
