17ca842aad4a1f9df378788eb2f41d6621e66a2a
Matt Soucy <msoucy@csh.rit.edu>
2015-04-13 00:47:43 -0400

    Split and cleanup setup

---
+++ setup-dependencies.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# CMake
+wget http://www.cmake.org/files/v3.1/cmake-3.1.2-Linux-x86_64.sh
+chmod +x cmake-3.1.2-Linux-x86_64.sh
+mkdir cmake
+sh cmake-3.1.2-Linux-x86_64.sh --skip-license --prefix=cmake --exclude-subdir
+
+# libglfw3 isn't up to date in apt with precise
+wget http://sourceforge.net/projects/glfw/files/glfw/3.0.4/glfw-3.0.4.zip
+unzip glfw-3.0.4.zip
+pushd glfw-3.0.4
+	cmake -DBUILD_SHARED_LIBS=ON -G "Unix Makefiles"
+	make
+	sudo make install
+	sudo cp /usr/local/lib/libglfw* /usr/lib/
+	sudo cp -R /usr/local/include/GLFW /usr/include/
+popd
+
+# SWIG
+wget http://prdownloads.sourceforge.net/swig/swig-3.0.5.tar.gz
+tar -xzf swig-3.0.5.tar.gz
+pushd swig-3.0.5
+	./configure
+	make
+	sudo make install
+popd
+
+# INI Parser
+wget http://ndevilla.free.fr/iniparser/iniparser-3.1.tar.gz
+tar -xzf iniparser-3.1.tar.gz
+pushd iniparser
+	make
+	sudo cp libiniparser.so.0 /usr/lib/libiniparser.so
+	sudo cp src/iniparser.h src/dictionary.h /usr/include/
+popd
+
+
